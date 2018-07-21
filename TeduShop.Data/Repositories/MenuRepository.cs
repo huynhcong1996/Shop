91ba55dede3b28c341182265867acf9d16824959
HuynhCong <huynhcong969@gmail.com>
2018-07-21 12:48:12 +0700

    update

--- TeduShop.Data/Repositories/MenuRepository.cs
+++ TeduShop.Data/Repositories/MenuRepository.cs
@@ -3,11 +3,11 @@ using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    public interface IMenuRepository : IRepository<Function>
+    public interface IMenuRepository : IRepository<Menu>
     {
     }
 
-    public class MenuRepository : RepositoryBase<Function>, IMenuRepository
+    public class MenuRepository : RepositoryBase<Menu>, IMenuRepository
     {
         public MenuRepository(IDbFactory dbFactory) : base(dbFactory)
         {
